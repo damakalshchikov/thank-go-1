a5bf2bfbe95d71f78bbc4529f86ff1c8c06be90e
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2026-01-19 03:32:36 +0300

    Рефакторинг

--- basic/basic1/distance.go
+++ basic/basic1/distance.go
@@ -6,7 +6,7 @@ import (
 )
 
 func DistanceBetweenPoints() {
-    // объявите переменные x1, y1, x2, y2 типа float64
+	// объявите переменные x1, y1, x2, y2 типа float64
 	var x1, y1, x2, y2 float64
 
 	// считываем числа из os.Stdin
@@ -17,7 +17,7 @@ func DistanceBetweenPoints() {
 	// рассчитайте d по формуле эвклидова расстояния
 	// используйте math.Pow(x, 2) для возведения в квардрат
 	// используйте math.Sqrt(x) для извлечения корня
-	d := math.Sqrt(math.Pow(x1 - x2, 2) + math.Pow(y1 - y2, 2))
+	d := math.Sqrt(math.Pow(x1-x2, 2) + math.Pow(y1-y2, 2))
 
 	// выводим результат в os.Stdout
 	fmt.Println(d)
