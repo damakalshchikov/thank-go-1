803187970434a136bc47e9d34b8ae224788256dc
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2026-01-19 03:33:50 +0300

    Добавлены задачи из раздела 1.2 "Базовые конструкции"

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "github.com/damakalshchikov/thank-go-1/basic/basic1"
 
 func main() {
-    // basic1.DurationInMinutes()
-    basic1.DistanceBetweenPoints()
+	// basic1.DurationInMinutes()
+	// basic1.DistanceBetweenPoints()
+	// basic1.RepeatString("a", 5)
+	basic1.LangByCode("en")
 }
