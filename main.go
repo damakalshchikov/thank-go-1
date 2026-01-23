c9a1d051b631906be56cd265ea2961caad50d58c
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2026-01-23 09:29:06 +0300

    Рефакторинг

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	// basic_constructions.LangByCode("en")
 
 	// Задачи из раздела 1.3 "Массивы и карты"
-	//arrays_and_maps.ToShortString("12345", 1)
+	// arrays_and_maps.ToShortString("12345", 1)
 	// arrays_and_maps.NumCounter(123)
 	// arrays_and_maps.MakeAbbreviation()
 }
